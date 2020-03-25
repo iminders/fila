6c1259742b915618b3e3274b3c154a68ee05e4c8
iminders <liuwen.w@qq.com>
2020-03-26 02:06:40 +0800

    init

---
+++ docker-compose.sh
@@ -0,0 +1,16 @@
+set -e
+cd "$(dirname "$0")"
+echo `pwd`
+
+export BAZEL_RUNID=$RANDOM
+echo $BAZEL_RUNID
+
+bazel --batch \
+      --output_user_root=/root/cache/bazel \
+      test --package_path=/root/fila \
+           --spawn_strategy=standalone \
+           --genrule_strategy=standalone \
+           --action_env="TUSHARE_TOKEN=$TUSHARE_TOKEN" \
+           --test_output=errors \
+           -c opt \
+           -- //...
